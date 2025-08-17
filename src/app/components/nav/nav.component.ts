46a73f68a7f29fe2c01143d28a26982d0da8c876
Guilherme Hirle <guilherme.hirle1@gmail.com>
2025-08-17 16:22:03 -0300

    ajuste de navbar fixo

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
   openClose: boolean = false;
   scrolled = false;
   previousScrollPosition = window.pageYOffset;
   navbarVisible = true;
   lastScrollTop = 0;
   message: string = '';
+  isMobile = false;
+
+  lastScrollPosition = 0;
+  isHeaderVisible = true;
+  headerHeight = 0;
 
   itemCount: number = 0;
-  private subscription: Subscription = new Subscription();
-  private cardItemsSubscription: Subscription = new Subscription();
 
-  constructor(private router: Router) {
- 
+  constructor(private router: Router, private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.checkMobile();
+
+    // Obtém a altura do header-menu após a view ser inicializada
+    const headerMenu = this.el.nativeElement.querySelector('[header-menu]');
+    if (headerMenu) {
+      this.headerHeight = headerMenu.offsetHeight;
+    }
   }
 
   ngOnInit(): void {
@@ -29,40 +39,47 @@ export class NavComponent implements OnInit {
     // this.subscription = this.cartService.itemCount$.subscribe(
     //   (count) => (this.itemCount = count)
     // );
-
-
   }
 
   public menu() {
     this.openClose = !this.openClose;
   }
 
+  @HostListener('window:resize')
+  onResize() {
+    this.checkMobile();
+  }
+
+  private checkMobile() {
+    this.isMobile = window.innerWidth < 768; // 768px é o breakpoint padrão do Tailwind para md
+    if (!this.isMobile) {
+      this.isHeaderVisible = true; // Garante que fique visível em desktop
+    }
+  }
+
   receiveMessage(event: string) {
     this.message = event;
   }
 
-  // @HostListener('window:scroll', [])
-  // onWindowScroll() {
-  //   const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (!this.isMobile) return;
 
-  //   if (currentScroll > this.lastScrollTop) {
-  //     // Rolando para baixo: esconde o navbar
-  //     this.scrolled = true;
-  //   } else {
-  //     // Rolando para cima: mostra o navbar
-  //     this.scrolled = false;
-  //   }
+    const currentScrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
 
-  //   this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-  // }
-  // ngOnDestroy(): void {
-  //   // Cancela a inscrição ao destruir o componente
-  //   this.subscription.unsubscribe();
-  // }
+    // Scroll DOWN - esconde o header
+    if (
+      currentScrollPosition > this.lastScrollPosition &&
+      currentScrollPosition > 50
+    ) {
+      this.isHeaderVisible = false;
+    }
+    // Scroll UP - mostra o header
+    else if (currentScrollPosition < this.lastScrollPosition) {
+      this.isHeaderVisible = true;
+    }
+
+    this.lastScrollPosition = currentScrollPosition;
+  }
 }
-
-
-
-
-
-
